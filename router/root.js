e2cd6cc4d1b4c9a20d6281f60895e5f7f5a3fd80
강백호 <162931494+kangback324@users.noreply.github.com>
2024-08-23 12:24:51 +0900

    Update root.js

--- router/root.js
+++ router/root.js
@@ -5,24 +5,11 @@ var router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    let check
-      const db = await pool.getConnection();
-      try {
-          [check] = await db.query('select admin from user where user_id=?',[req.session.user_id]);
-      } catch (err) {
-          console.log(err);
-      } finally {
-          db.release();
-      }
-
     const owner = await isowner.isowner(req);
-    if(check[0].admin === 'Y') {
-      console.log('ss')
-      res.redirect('/admin');
-    } 
-    else if (owner) {
+    if (owner) {
       res.redirect("/stock");
-    } else {
+    } 
+    else {
       res.redirect("/login");
     }
   } catch (err) {
